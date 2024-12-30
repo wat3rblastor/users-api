a369f1528de2f2d3d0c85c66c93bfa70943f29cd
wat3rblastor <bowencheng2027@u.northwestern.edu>
2024-12-29 20:39:20 -0600

    add more api endpoints

---
+++ README.md
@@ -0,0 +1,13 @@
+## Approach
+
+I focused on designing each API endpoint individually, iterating on the functionality as needed. This method allowed me to efficiently develop and adapt each endpoint by building on existing structures.
+
+## Takeaways
+
+I learned the importance of hiding sensitive variables in a .env file. Moving forward, I’ll ensure to apply this practice across all my projects for better security and maintainability.
+
+## Challenges
+
+One challenge was standardizing error codes across the application, which required careful planning and implementation.
+
+I also had to create a frontend from scratch, as I only discovered DISC through an email about the Discover program. The primary difficulty in building the frontend was deciding which state variables to use, leading to several changes in implementation before finding the right approach. Additionally, I learned that .env variables for the frontend must start with NEXT_PUBLIC_, which took some time to figure out.
--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to your Express server" });
 });
 
-app.get("/api/users", async (req, res) => {
+app.get("/users", async (req, res) => {
     try {
         const result = await pool.query(
             "SELECT * FROM users ORDER BY id ASC;"
@@ -22,10 +22,81 @@ app.get("/api/users", async (req, res) => {
         res.json(result.rows);
     } 
     catch (err) {
-        console.log("Query error:", err);
-        res.status(500).json({ error: "Failed to fetch users" });
+        console.log("GET users error:", err);
+        res.status(500).json({ "msg": "Failed to fetch users" });
     }
 
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const { first_name, last_name, email, date_of_birth } = req.body;
+    const result = await pool.query(
+      "INSERT INTO users(first_name, last_name, email, date_of_birth) VALUES ($1, $2, $3, $4);",
+      [first_name, last_name, email, date_of_birth]
+    );
+    res.json({ "msg": "Success!" })
+  }
+  catch (err) {
+    console.log("POST users error:", err);
+    res.status(500).json({ "msg": "Failed to create new user" });
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user_id = req.params.id;
+    const isInDB = await pool.query(
+      "SELECT * FROM users WHERE id = $1;",
+      [user_id]
+    );
+
+    if (isInDB.rows.length === 0) {
+      res.status(400).json({"msg": "User does not exist"});
+      return;
+    }
+
+    const result = await pool.query(
+      "DELETE FROM users WHERE id = $1;",
+      [user_id]
+    );
+    res.json({ "msg": "Success!" });
+  }
+  catch (err) {
+    console.log("DELETE error:", err);
+    res.status(500).json({ "msg": "Failed to delete user" });
+  }
+});
+
+app.put("/users/:id", async (req, res) => {
+  try {
+    const user_id = req.params.id;
+    const { first_name, last_name, email, date_of_birth } = req.body;
+    const isInDB = await pool.query(
+      "SELECT * FROM users WHERE id = $1;",
+      [user_id]
+    );
+
+    if (isInDB.rows.length === 0) {
+      res.status(400).json({"msg": "User does not exist"});
+      return;
+    }
+
+    const result = await pool.query(
+      `
+       UPDATE users
+       SET first_name=$1, last_name=$2, email=$3, date_of_birth=$4
+       WHERE id=$5 
+      `,
+      [first_name, last_name, email, date_of_birth, user_id]
+    )
+    res.json({ "msg": "Success!" });
+
+  } 
+  catch (err) {
+    console.log("PUT error:", err);
+    res.status(500).json({ error: "Failed to update user" });
+  }
 });
 
 const PORT = process.env.PORT || 3005;
